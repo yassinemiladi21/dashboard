cc085690077a47af933304c90fa3cf2f5c53bf16
= <=>
2024-06-03 00:39:06 +0100

    setting spring batch processor

---
+++ src/main/java/miladi/yassine/dashboard/data/MatchDataProcessor.java
@@ -0,0 +1,39 @@
+package miladi.yassine.dashboard.data;
+
+
+import java.time.LocalDate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+import miladi.yassine.dashboard.model.Match;
+
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
+
+  private static final Logger log = LoggerFactory.getLogger(MatchDataProcessor.class);
+
+  @Override
+  public Match process(final MatchInput matchInput) throws Exception {
+
+    Match match = new Match();
+    match.setId(Long.parseLong(matchInput.getId()));
+    match.setSeason(matchInput.getSeason());
+    match.setCity(matchInput.getCity());
+    match.setDate(LocalDate.parse(matchInput.getDate()));
+    match.setPlayerOfMatch(matchInput.getPlayer_of_match());
+    match.setVenue(matchInput.getVenue());
+    match.setTeam1(matchInput.getTeam1());
+    match.setTeam2(matchInput.getTeam2());
+    match.setTossWinner(matchInput.getToss_winner());
+    match.setTossDecision(matchInput.getToss_decision());
+    match.setMatchWinner(matchInput.getWinner());
+    match.setResult(matchInput.getResult());
+    match.setResultMargin(matchInput.getResult_margin());
+    match.setUmpire1(matchInput.getUmpire1());
+    match.setUmpire2(matchInput.getUmpire2());
+
+    return match;
+  }
+
+}
